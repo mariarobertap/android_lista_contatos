f7bdd34e0b4c2913f8df4221ade2d7487d862ce5
Maria Roberta <mariarobertaprado@outlook.com>
2022-05-16 23:10:49 -0300

    feat: arrumando os botoes, e adicionando notificação

--- app/src/main/java/com/example/listacontatos/MainActivity.java
+++ app/src/main/java/com/example/listacontatos/MainActivity.java
@@ -5,16 +5,23 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.ContentValues;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -29,6 +36,8 @@ public class MainActivity extends AppCompatActivity {
 
     private SQLiteDatabase db;
     private AlertDialog adAlterarExcluir;
+    private Notification notification;
+    private NotificationManager manager;
 
     @SuppressLint("Range")
     @Override
@@ -68,12 +77,76 @@ public class MainActivity extends AppCompatActivity {
                                     //"Rua das Flores, 542",
                                    // "41234354",
                                    // "4499875634"));
+
+        manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        Notification.Builder builder = new Notification.Builder(this)
+                .setContentTitle("Novo site do integrado")
+                .setContentText("Veja o novo site da faculdade")
+                .setAutoCancel(true)
+                .setSmallIcon(R.drawable.ic_baseline_web_24);
+
+        Uri webpage = Uri.parse("https://grupointegrado.br");
+        Intent webIntent = new Intent(Intent.ACTION_VIEW, webpage);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this,
+                0,
+                webIntent,
+                0);
+
+        builder.setContentIntent(pendingIntent);
+
+        notification = builder.build();
+
+        manager.notify(R.drawable.ic_launcher_background, notification);
+
         adptador = new ContatoAdapter(this, 0, listaDinamica);
         listView = (ListView) findViewById(R.id.Dinamico);
         listView.setAdapter(adptador);
         listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
             @Override
             public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+                LayoutInflater layoutInflater = getLayoutInflater();
+                View alterarExcluir = layoutInflater.inflate(R.layout.alterar_excluir, null);
+                alterarExcluir.findViewById(R.id.imFundo);
+                TextView text = alterarExcluir.findViewById(R.id.tvTitulo);
+
+                text.setText("cLIEUQ EM ALETRAR PARA MODIFICAR");
+                alterarExcluir.findViewById(R.id.btAlterar).setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Intent intent = new Intent(MainActivity.this, adicionarContato.class);
+                        Bundle bundle = new Bundle();
+                        bundle.putInt("posicao", i);
+                        bundle.putString("nome", listaDinamica.get(i).getNome());
+                        bundle.putString("endereco", listaDinamica.get(i).getEndereco());
+                        bundle.putString("telefone1", listaDinamica.get(i).getTelefone1());
+                        bundle.putString("telefone2", listaDinamica.get(i).getTelefone2());
+                        intent.putExtras(bundle);
+                        startActivityForResult(intent, ALTERAR_CONTATO);
+                        adAlterarExcluir.dismiss();
+
+                    }
+                });
+                alterarExcluir.findViewById(R.id.btExcluir).setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        if(!listaDinamica.isEmpty()){
+                            int idx = listaDinamica.get(i).getId();
+                            db.delete("CONTATOS", "ID = ?", new String[]{String.valueOf(idx)});
+                            listaDinamica.remove(i);
+                            adptador.notifyDataSetChanged();
+                            adAlterarExcluir.dismiss();
+                        }
+                    }
+                });
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setTitle("Alterar ou Excluir?");
+                builder.setView(alterarExcluir);
+                adAlterarExcluir = builder.create();
+                adAlterarExcluir.show();
+                return false;
+                /*
                 AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this)
                 .setTitle("Alterar ou Excluir")
                 .setMessage("Clique em alterar para modificar os dados ou excluir para remomer o contato")
@@ -103,12 +176,19 @@ public class MainActivity extends AppCompatActivity {
                             }
                         });
 
+
+
+
                 adAlterarExcluir = builder.create();
                 adAlterarExcluir.show();
 
                 return false;
+                 */
+
             }
         });
+
+
 
 
 
